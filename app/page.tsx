72c913b9660ebbc30ac0ede80df98561133a7615
Vishal Nayak <vishnaya@amazon.com>
2025-01-08 10:47:31 +0530

    frontend change

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from "react";
+import "./../app/app.css";
 
 Amplify.configure(outputs);
 
@@ -44,7 +44,7 @@ export default function App() {
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
+          Please review next steps of this tutorial.
         </a>
       </div>
     </main>
