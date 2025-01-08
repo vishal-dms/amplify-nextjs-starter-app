2c56b9ada006a4fccb38f56969df6b260014ed43
Vishal Nayak <vishnaya@amazon.com>
2025-01-08 10:57:59 +0530

    backend change

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,82 +8,6 @@ const backend = defineBackend({
   data,
 });
 
-// export const configureResolverCache = (
-//   resolver: appsync.CfnResolver,
-//   config: IResolverCacheConfig = {}
-// ): void => {
-//   if (!ENABLE_APPSYNC_CACHE) {
-//     return;
-//   }
-
-//   const { ttl = 300, cachingKeys = ["$context.arguments.id"] } = config;
-
-//   if (!resolver) {
-//     throw new Error(resolver);
-//   }
-
-//   resolver.cachingConfig = {
-//     ttl,
-//     cachingKeys,
-//   };
-// };
-
-// const RESOLVER_GET_MENU_SECTIONS_FOR_RESTAURANT =
-//   backend.data.resources.cfnResources.cfnResolvers[
-//     "Query.getMenuSectionsForRestaurant"
-//   ];
-
-// const RESOLVER_GET_MENU_PICKER_FOR_RESTAURANT =
-//   backend.data.resources.cfnResources.cfnResolvers[
-//     "Query.getMenuPickerForRestaurant"
-//   ];
-
-// configureResolverCache(RESOLVER_GET_MENU_SECTIONS_FOR_RESTAURANT, {
-//   ttl: 300,
-//   cachingKeys: ["$context.arguments.id"],
-// });
-
-// configureResolverCache(RESOLVER_GET_MENU_PICKER_FOR_RESTAURANT, {
-//   ttl: 300,
-//   cachingKeys: ["$context.arguments.id"],
-// });
-
-// export const createApiCache = (
-//   construct: Construct,
-//   identifier: string,
-//   apiId: string,
-//   config: IApiCacheConfig = {}
-// ): CfnApiCache | void => {
-//   if (!ENABLE_APPSYNC_CACHE) {
-//     return;
-//   }
-
-//   const {
-//     ttl = 300,
-//     type = "SMALL",
-//     apiCachingBehavior = "PER_RESOLVER_CACHING",
-//     atRestEncryptionEnabled = true,
-//     transitEncryptionEnabled = true,
-//   } = config;
-
-//   const stack = Stack.of(construct);
-
-//   return new CfnApiCache(stack, identifier, {
-//     apiId,
-//     ttl,
-//     type,
-//     apiCachingBehavior,
-//     atRestEncryptionEnabled,
-//     transitEncryptionEnabled,
-//   });
-// };
-
-// createApiCache(backend.data, "MyApiCache", backend.data.apiId, {
-//   ttl: 300,
-//   type: "SMALL",
-//   apiCachingBehavior: "PER_RESOLVER_CACHING",
-// });
-
 const cfnApiCache = new appsync.CfnApiCache(backend.stack, "MyCfnApiCache", {
   apiCachingBehavior: "PER_RESOLVER_CACHING",
   apiId: backend.data.resources.graphqlApi.apiId,
@@ -95,12 +19,18 @@ const cfnApiCache = new appsync.CfnApiCache(backend.stack, "MyCfnApiCache", {
   transitEncryptionEnabled: true,
 });
 
-const LIST_TODO =
-  backend.data.resources.cfnResources.cfnResolvers["Query.listTodos"];
+const GET_TODO =
+  backend.data.resources.cfnResources.cfnResolvers["Query.getTodo"];
 
-LIST_TODO.cachingConfig = {
-  cachingKeys: ["$context.arguments.limit", "$context.arguments.nextToken"],
+GET_TODO.cachingConfig = {
+  cachingKeys: ["$context.arguments.id"],
   ttl: 60, // Cache TTL in seconds
 };
 
-// LIST_TODO.addOverride("DependsOn", cfnApiCache.logicalId);
+const LIST_TODOS =
+  backend.data.resources.cfnResources.cfnResolvers["Query.listTodos"];
+
+LIST_TODOS.cachingConfig = {
+  cachingKeys: ["$context.arguments.limit", "$context.arguments.nextToken"],
+  ttl: 60, // Cache TTL in seconds
+};
